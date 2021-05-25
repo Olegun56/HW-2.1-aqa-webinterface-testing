e390d3cfa77f92840f975f7babfab83b0288d6bf
Oleg Esipov <oleg.esipov@gmail.com>
2021-05-25 17:38:41 +0500

    changes tests

--- src/test/java/DebitCardFormTest.java
+++ src/test/java/DebitCardFormTest.java
@@ -23,11 +23,11 @@ public class DebitCardFormTest {
 
     @BeforeEach
     void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        //  ChromeOptions options = new ChromeOptions();
+        //   options.addArguments("--disable-dev-shm-usage");
+        //   options.addArguments("--no-sandbox");
+        // options.addArguments("--headless");
+        driver = new ChromeDriver();
     }
 
     @AfterEach
