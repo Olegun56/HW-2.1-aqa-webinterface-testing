045073eb82eb094c9a9fdaa75325bbb8f18a889a
Oleg Esipov <oleg.esipov@gmail.com>
2021-05-25 22:10:44 +0500

    delete path to driver

--- src/test/java/DebitCardFormTest.java
+++ src/test/java/DebitCardFormTest.java
@@ -20,12 +20,6 @@ public class DebitCardFormTest {
     @BeforeAll
     static void setUpAll() {
         WebDriverManager.chromedriver().setup();
-        String os = System.getProperty("os.name").toLowerCase();
-        if (os.contains("windows")) {
-            System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
-        } else if (os.contains("linux")) {
-            System.setProperty("webdriver.chrome.driver", "./driver/linux/chromedriver");
-        }
     }
 
     @BeforeEach
